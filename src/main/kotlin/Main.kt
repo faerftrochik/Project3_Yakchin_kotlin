3002141656ee4b28bbd77c41f6596b32477b01e0
Souls Rider <faerftrochik@gmail.com>
2025-11-29 16:33:17 +0300

    4 Nachalo raboti

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>) {
     println("Пятая задача - 5")
     println("Шестая задача - 6")
     println("Введите задачу какую хотите увидеть - ")
+
     val inp = readLine()
 
     when (inp)
@@ -17,9 +18,6 @@ fun main(args: Array<String>) {
         "1" -> teat1()
         "2" -> teat2()
         "3" -> teat3()
-        "4" -> teat4()
-        "5" -> teat5()
-        "6" -> teat6()
 
         else -> print("Ввести надо одну цифру...")
     }
@@ -29,7 +27,16 @@ fun teat1()
 {
     //Задание 1
 
-    println("Введите строку:")
+    println("Приветствую в игре камень, ножницы, бумага")
+    println("Список возможностей: ")
+    println("1 - это камень")
+    println("2 - ножницы")
+    println("3 - бумага")
+    println("Правила игры:")
+    println("Бумага побеждает камень («бумага обёртывает камень»).\n Камень побеждает ножницы («камень затупляет или ломает ножницы»). \n" +
+            "Ножницы побеждают бумагу («ножницы разрезают бумагу»). \n" +
+            "Если игроки показали одинаковый знак, то засчитывается ничья и игра переигрывается.")
+    println("Хотите начать игру? => ")
     val input = readLine() ?: ""
     if (input.isEmpty()) {
         println("Строка пустая")
@@ -96,104 +103,5 @@ fun teat3()
         println("В двоичной системе: ${number.toString(2)}")
     } catch (e: Exception) {
         println("Ошибка ввода: введите целое число")
-    }
-}
-
-fun teat4()
-{
-    //Задание 4
-
-    println("Введите выражение (число1 число2 операция):")
-    val input = readLine() ?: ""
-
-    val parts = input.trim().split(Regex("\\s+"))
-    if (parts.size != 3) {
-        println("Неверный ввод. Введите: число1 число2 операция")
-        return
-    }
-
-    try {
-        val a = parts[0].toDouble()
-        val b = parts[1].toDouble()
-        val op = parts[2]
-
-        val result = when (op) {
-            "+" -> println("Результат: ${a + b}")
-            "-" -> println("Результат: ${a - b}")
-            "*" -> println("Результат: ${a * b}")
-            "/" -> if (b != 0.0) println("Результат: ${a / b}") else "Деление на ноль"
-            else -> "Неизвестная операция"
-        }
-
-        println("Результат: $result")
-    } catch (e: Exception) {
-        println("Ошибка ввода")
-    }
-}
-
-fun teat5()
-{
-    //Задание 5
-
-    println("Введите число n:")
-    val nInput = readLine()
-    println("Введите основание степени x:")
-    val xInput = readLine()
-
-    try {
-        val n = nInput?.toInt() ?: 0
-        val x = xInput?.toInt() ?: 0
-
-        var y = 0
-        var pow = 1
-        var found = false
-
-        while (pow <= n) {
-            if (pow == n) {
-                found = true
-                break
-            }
-            y++
-            pow *= x
-        }
-
-        if (found) {
-            println("Целочисленный показатель y = $y")
-        } else {
-            println("Целочисленный показатель не существует")
-        }
-    } catch (e: Exception) {
-        println("Ошибка ввода")
-    }
-}
-
-fun teat6()
-{
-    //Задание 6
-
-    println("Введите первую цифру:")
-    val aInput = readLine()
-    println("Введите вторую цифру:")
-    val bInput = readLine()
-
-    try {
-        val a = aInput?.toInt() ?: 0
-        val b = bInput?.toInt() ?: 0
-
-        if (a == b) {
-            println("Цифры должны быть разными")
-            return
-        }
-
-// число нечётное если последняя цифра нечётная
-        if (a % 2 == 1) {
-            println("Результат: ${b * 10 + a}")
-        } else if (b % 2 == 1) {
-            println("Результат: ${a * 10 + b}")
-        } else {
-            println("Создать нечетное число невозможно")
-        }
-    } catch (e: Exception) {
-        println("Ошибка ввода")
     }
 }
